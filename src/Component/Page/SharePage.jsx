692caa69afda2cf2de3b6eb4aeb36386577947f8
Arnob004 <arnobpandit2023@gmail.com>
2025-10-03 22:15:15 +0530

    Fix import path casing for Vercel deployment

--- src/Component/Page/SharePage.jsx
+++ src/Component/Page/SharePage.jsx
@@ -6,17 +6,17 @@ import { Link, useLocation } from "react-router-dom";
 
 const SharePage = () => {
     // get scanned/connected user data from router state
-    const location = useLocation();
-    const { currentUser, connectedUser } = location.state || {
-        currentUser: {
-            name: "Me",
-            photo: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
-        },
-        connectedUser: {
-            name: "Unknown",
-            photo: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
-        },
-    };
+    // const location = useLocation();
+    // const { currentUser, connectedUser } = location.state || {
+    //     currentUser: {
+    //         name: "Me",
+    //         photo: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    //     },
+    //     connectedUser: {
+    //         name: "Unknown",
+    //         photo: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    //     },
+    // };
     return (
         <div>
             <div className="w-full h-screen flex justify-center items-center p-4 bg-zinc-900">
@@ -32,22 +32,22 @@ const SharePage = () => {
                         <div className="w-full flex justify-around items-center">
                             {/* current user */}
                             <div className="capitalize flex gap-2.5 items-center">
-                                <img
+                                {/* <img
                                     className="w-10 h-10 rounded-full"
                                     src={currentUser.photo}
                                     alt={currentUser.name}
                                 />
-                                {currentUser.name}
+                                {currentUser.name} */}
                             </div>
 
                             {/* connected user */}
                             <div className="flex capitalize gap-2.5 items-center">
-                                {connectedUser.name}
-                                <img
+                                {/* {connectedUser.name} */}
+                                {/* <img
                                     className="w-10 h-10 rounded-full"
                                     src={connectedUser.photo}
                                     alt={connectedUser.name}
-                                />
+                                /> */}
                             </div>
                         </div>
                     </div>
@@ -55,7 +55,7 @@ const SharePage = () => {
                     {/* middle (chat area) */}
                     <div className="middle flex-grow bg-zinc-800 p-4 overflow-auto text-white">
                         <p className="text-center text-sm text-zinc-400">
-                            Connected with <span className="font-bold">{connectedUser.name}</span>
+                            {/* Connected with <span className="font-bold">{connectedUser.name}</span> */}
                         </p>
                         {/* Chat messages will come here */}
                     </div>
@@ -67,7 +67,7 @@ const SharePage = () => {
                         </button>
                         <input
                             type="text"
-                            placeholder={`Message ${connectedUser.name}...`}
+                            // placeholder={`Message ${connectedUser.name}...`}
                             className="flex-grow bg-zinc-700 text-white rounded-md px-3 py-2 outline-none border border-zinc-500 focus:border-amber-400 transition"
                         />
                         <button className="w-12 h-8 rounded-md flex justify-center items-center bg-amber-400 hover:bg-amber-300 transition">
